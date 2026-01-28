2db0e9ba08004be73ec1464e48927c78df460018
Ackerman, Adam P. <adam@ou.edu>
2026-01-28 12:42:02 -0600

    completed in class example over functions

---
+++ In_Class_Examples/Review_Functions/Program.cs
@@ -0,0 +1,23 @@
+﻿
+double yVal1 = LineValueForY(2.0, 3.0, 4.0);
+
+int fact5 = Factorial(5);
+
+
+static double LineValueForY (double m, double x, double b)
+{
+    double yValue = m * x + b;
+    return yValue;
+}
+
+static int Factorial(int n)
+{
+    int result = 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        result *= i;
+    }
+
+    return result;
+}
