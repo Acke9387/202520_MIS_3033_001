e7656eb0df69cc45e0bbb17422e39e9a6e1f7d66
Ackerman, Adam P. <adam@ou.edu>
2026-01-30 12:49:32 -0600

    Completed in a class example over wpf

---
+++ In_Class_Examples/WPF_First_Application/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_First_Application
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            txtLastName.Text = "";
+            txtFirstName.Focus();
+        }
+
+        //private void Button_MouseEnter(object sender, MouseEventArgs e)
+        //{
+        //    MessageBox.Show("Mouse Entered the Button Area","my caption", MessageBoxButton.AbortRetryIgnore);
+        //}
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = txtFirstName.Text;
+            string lastName = txtLastName.Text;
+
+            MessageBox.Show($"Hello {firstName} {lastName}","Greeting", MessageBoxButton.OK);
+
+            txtLastName.Clear();
+            txtFirstName.Text = string.Empty;
+        }
+    }
+}
---
+++ In_Class_Examples/WPF_First_Application/Toy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_First_Application
+{
+    public class Toy
+    {
+        public string Manufactrer { get; set; }
+        public string Name { get; set; }
+
+        private string Aisle { get; set; }
+        public double Price { get; set; }   
+        public List<string> Tags { get; set; }
+
+        public Toy()
+        {
+            Manufactrer = "";
+            Price = 0.0;
+            Name = string.Empty;
+            Aisle = "";
+            Tags = new List<string>();
+        }
+
+
+    }
+}
