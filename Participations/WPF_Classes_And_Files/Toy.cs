bde913227ee6850d8ac5fb90d7d09014f8a95a0d
Ackerman, Adam P. <adam@ou.edu>
2026-02-02 12:14:06 -0600

    started participation for classes and files

---
+++ Participations/WPF_Classes_And_Files/Toy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_Classes_And_Files
+{
+    internal class Toy
+    {
+
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public string Image { get; set; }
+
+        private string Aisle;
+
+        public Toy()
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Price = 0.0;
+            Image = string.Empty;
+            Aisle = string.Empty;
+        }
+
+        public string GetAisle()
+        {
+            string aisle;
+
+            aisle = "" + Manufacturer.ToUpper()[0].ToString();
+            aisle += Price.ToString().Replace(".", "").Replace(",", "");
+            return aisle;
+        }
+
+    }
+}
