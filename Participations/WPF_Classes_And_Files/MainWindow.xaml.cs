e457330a4017d55aa67134ec5eb803f374769751
Ackerman, Adam P. <adam@ou.edu>
2026-02-02 12:24:33 -0600

    added reading in the file

--- Participations/WPF_Classes_And_Files/MainWindow.xaml.cs
+++ Participations/WPF_Classes_And_Files/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,28 @@ namespace WPF_Classes_And_Files
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Toy> toys = new List<Toy>();
         public MainWindow()
         {
             InitializeComponent();
+            string[] linesOfFile = File.ReadAllLines("Toys.csv");
+
+            foreach (string line in linesOfFile)
+            {
+                //line = Manufacturer,Name,Price,Image
+                string[] partsOfLine = line.Split(',');
+                Toy myToy = new Toy();
+                myToy.Manufacturer = partsOfLine[0];
+                myToy.Name = partsOfLine[1];
+                myToy.Price = double.Parse(partsOfLine[2]);
+                myToy.Image = partsOfLine[3];
+                //partsOfLine[0] = Manufacturer
+                //partsOfLine[1] = Name
+                //partsOfLine[2] = Price
+                //partsOfLine[3] = Image
+                toys.Add(myToy);
+            }
+
         }
     }
 }
