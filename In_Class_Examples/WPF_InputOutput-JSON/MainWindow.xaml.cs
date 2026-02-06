3ab2b3546a5847dff34f2e117f59304843cec452
Ackerman, Adam P. <adam@ou.edu>
2026-02-06 12:58:26 -0600

    Completed in class example over input/output and JSON

---
+++ In_Class_Examples/WPF_InputOutput-JSON/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_InputOutput_JSON
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Snack> snacks = new List<Snack>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            string fileContent = File.ReadAllText("Superbowl_Snacks.json");
+            snacks = JsonConvert.DeserializeObject<List<Snack>>(fileContent);
+
+            cboSpicyLevel.Items.Add("All levels");
+            foreach (Snack item in snacks)
+            {
+                if (cboSpicyLevel.Items.Contains(item.spiciness_level) == false)
+                {
+                    cboSpicyLevel.Items.Add(item.spiciness_level);
+                }
+
+                lstSnacks.Items.Add(item);
+            }
+
+        }
+
+        private void cboSpicyLevel_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string spice_level = cboSpicyLevel.SelectedItem.ToString();
+
+            lstSnacks.Items.Clear();
+
+            foreach (Snack item in snacks)
+            {
+                if (item.spiciness_level == spice_level ||
+                    spice_level == "All levels")
+                {
+                    lstSnacks.Items.Add(item);
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string fileName = $"{cboSpicyLevel.SelectedItem.ToString()}_data.json";
+
+            string fileContentsAsJson = JsonConvert.SerializeObject(lstSnacks.Items, Formatting.Indented);
+
+            File.WriteAllText(fileName, fileContentsAsJson);
+            MessageBox.Show($"Successfully saved your file: {fileName}");
+        }
+    }
+}
---
+++ In_Class_Examples/WPF_InputOutput-JSON/Snack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPF_InputOutput_JSON
+{
+    public class Snack
+    {
+        public string snack_name { get; set; }
+        public double price { get; set; }
+        public double calories { get; set; }
+        public double serving_size { get; set; }
+        public string spiciness_level { get; set; }
+
+        public Snack()
+        {
+            snack_name = string.Empty;
+            price = 0.0;
+            calories = 0.0;
+            serving_size = 0.0;
+            spiciness_level = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return $"{spiciness_level} - {snack_name}";
+        }
+
+    }
+}
