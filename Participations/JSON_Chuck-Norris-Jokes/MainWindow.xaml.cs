fb1f009d75aadcf574b7eb3c8f5269eb404e6b01
Ackerman, Adam P. <adam@ou.edu>
2026-03-04 12:19:20 -0600

    started chuck norris participation

---
+++ Participations/JSON_Chuck-Norris-Jokes/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JSON_Chuck_Norris_Jokes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string url = "https://api.chucknorris.io/jokes/categories";
+            cboCategories.Items.Add("All Categories");
+
+            using(HttpClient client = new HttpClient())
+            {
+                string json = client.GetStringAsync(url).Result;
+                List<string> categories = JsonConvert.DeserializeObject<List<string>>(json);
+                foreach (string category in categories)
+                {
+                    cboCategories.Items.Add(category);
+                }
+            }
+            cboCategories.SelectedIndex = 0;
+        }
+    }
+}
